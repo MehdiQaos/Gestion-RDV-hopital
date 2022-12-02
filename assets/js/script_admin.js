4ffe3743caa3f0d4ac792f3801e6aadd34968e78
Ossama kharbaq <ossamakharbaq4@gmail.com>
2022-12-02 17:07:48 +0100

    finished the sign pages and 90% of the admin dashboard, still  need to add redirection to show all appointments and sessions

--- assets/js/script_admin.js
+++ assets/js/script_admin.js
@@ -54,3 +54,27 @@
         patients.click(function(){
             displayThis(patientsSection,dashboardSection,doctorsSection,scheduleSection,appointmentSection);
         });
+
+
+
+let update = $('#update-btn');
+let addDoctor = $('#add-doctor-btn');
+
+let updateTitle = $('#edit-title');
+let addTitle = $('#add-title');
+
+let updateBtn = $('#doctor-update-btn');
+let saveBtn = $('#doctor-save-btn');
+
+update.click(function(){
+    addTitle.addClass('d-none');
+    saveBtn.addClass('d-none');
+    updateTitle.removeClass('d-none');
+    updateBtn.removeClass('d-none');
+})
+addDoctor.click(function(){
+    addTitle.removeClass('d-none');
+    saveBtn.removeClass('d-none');
+    updateTitle.addClass('d-none');
+    updateBtn.addClass('d-none');
+})
