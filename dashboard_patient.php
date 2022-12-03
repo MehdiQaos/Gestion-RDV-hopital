c6d3a3b7c67c26da99ae3750d58dc6e9dfe9fff7
karim elaoumari <elaoumarikarim@gmail.com>
2022-12-03 16:50:14 +0100

    added the best pagination to project

---
+++ dashboard_patient.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link rel="stylesheet" href="assets/css/style_patient.css" />
+    <title>Hopital system management</title>
+</head>
+
+<body>
+    <div class="d-flex shadow-sm bg-light" id="wrapper" >
+        <!-- Sidebar -->
+        <div class="bg-white" id="sidebar-wrapper">
+            <div class="sidebar-heading text-center d-flex flex-column py-4 fs-5 border-bottom mt-5">
+                <div class="d-flex align-items-center">
+                    <img src="img/photos/user.png" width="40px" class="rounded-circle me-3" alt="">
+                    <div>
+                        <p style="margin:-5px;">admin</p>
+                        <p class="text-secondary fs-6">admin@admin.com</p>
+                    </div>
+                </div>
+                    <button class="btn btn-lg btn-block btn-light my-3 mycolor button1 fs-6 " type="button">Log out</button> </div>
+            <div class="list-group list-group-flush my-3">
+                <a href="dashboard_patient.php?file=dash" class="list-group-item list-group-item-action  text-secondary " onclick="show_dash()"><i
+                        class="uil uil-home fs-4 me-2"></i>Home</a>
+                <a  href="dashboard_patient.php?file=doct" class="list-group-item list-group-item-action   fw-bold" onclick="show_doct()"><i
+                        class="uil uil-medkit me-2 fs-4" ></i>All Doctors</a>
+                <a  href="dashboard_patient.php?file=session" class="list-group-item list-group-item-action  fw-bold" onclick="show_sess()"><i
+                        class="uil uil-stopwatch me-2 fs-4" ></i>Scheduled Sessions</a>        
+                <a  href="dashboard_patient.php?file=appoint" class="list-group-item list-group-item-action   fw-bold"onclick="show_booking()"><i
+                        class="uil uil-bookmark me-2 fs-4"></i>My Booking</a>
+                <a  href="dashboard_patient.php?file=settings" class="list-group-item list-group-item-action fw-bold" onclick="show_settings()"><i
+                        class="uil uil-setting fs-4 me-2"></i>Settings</a>
+            </div>
+        </div>
+        <!-- /#sidebar-wrapper -->
+        <!-- Page Content -->
+        <div id="page-content-wrapper" style="height: 100vh; overflow: scroll;">
+            
+        <?php
+         if(isset($_GET["file"])){
+            if($_GET['file']=="dash"){
+                include("includes/patient_includes/patient_dash.php");
+                }
+                else if($_GET['file']=="doct"){
+                    include("includes/patient_includes/patient_doct.php");
+                }
+                else if($_GET['file']=="session"){
+                    include("includes/patient_includes/patient_session.php");
+               }
+               else if($_GET['file']=="appoint"){
+                  include("includes/patient_includes/patient_apoint.php");
+               }
+               else{
+                  include("includes/patient_includes/patient_settings.php");
+               } 
+         }
+         else{
+            include("includes/patient_includes/patient_dash.php");
+
+         }
+        ?>
+        </div>
+<!-- forms that should be in a separate fille includable -->
+                   <div class="modal fade" id="modal-doctor">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <form action="" method="POST" name="form_add_doctor">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">Add New Doctor</h5>
+                                    <a href="#" class="btn-close" data-bs-dismiss="modal"></a>
+                                </div>
+                                <div class="modal-body">
+                                        <input type="hidden" id="doctor-id">
+                                        <div class="mb-3">
+                                            <label class="form-label">Doctor Name</label>
+                                            <input type="text" name="doctorname" class="form-control" id="doctor-name"/>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="form-label">Email</label>
+                                            <input type="text" name="doctoremail" class="form-control" id="doctor-email"/>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="form-label">Speciality</label>
+                                            <input type="text" name="speciality" class="form-control" id="doctor-speciality"/>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="form-label">Password</label>
+                                            <input type="text" name="doctorpassword" class="form-control" id="doctorpassword"/>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="form-label">Phone Number</label>
+                                            <input type="text" name="doctornumber" class="form-control" id="doctornumber"/>
+                                        </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <a href="#" class="btn btn-white" data-bs-dismiss="modal">Cancel</a>
+                                    <button type="submit" name="savedoctor" class="btn mycolor button1" id="doctor-save-btn">Save</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+    <!-- /#page-content-wrapper -->
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        var el = document.getElementById("wrapper");
+        var toggleButton = document.getElementById("menu-toggle");
+
+        toggleButton.onclick = function () {
+            el.classList.toggle("toggled");
+        };
+        var toggleButtonn = document.getElementById("menu-togglee");
+
+        toggleButtonn.onclick = function () {
+            el.classList.toggle("toggled");
+        };
+    </script>
+    <script src="assets/js/script_patient.js"></script>
+</body>
+
+</html>
---
+++ includes/patient_includes/patient_doct.php
@@ -0,0 +1,52 @@
+<section class="pt-2 " id="doctors" style="">
+                    
+                    <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4 justify-content-between" id="dashboard">
+                        <div class="d-flex align-items-center">
+                            <i class="fas fa-align-left mycolor fs-4 " id="menu-togglee"></i>
+                            <!-- <h2 class="fs-2 m-0"> </h2> -->
+                            <div class="input-group ms-5" style="width:500px;">
+                                <input type="search" class="form-control rounded" placeholder="Search Doctor Name or Email" aria-label="Search" aria-describedby="search-addon" />
+                                <button type="button" class="btn btn-secondary mycolor ms-2 rounded button1 border-0">search</button>
+                            </div>
+                        </div>
+                    <div class="d-flex align-items-center end-50">
+                        <div class="pt-3 me-3">
+                          <p class="fs-6 text-secondary" style="margin-bottom: -5px;">Today's date</p>
+                          <p class="fs-6 text-black fw-bold"><?= date("Y-m-d") ?></p>
+                        </div>
+                        <i class="uil uil-calendar-alt fs-2 mt-1 box rounded p-2"></i>
+                    </div>
+                    </nav>        
+                        <div class="d-flex justify-content-between">
+                            <p class="fs-5 ms-4">All Doctors(0)</p>
+                        </div>
+                         <div class="card-body table-responsive mt-2" style="height: 75vh; overflow: scroll;">
+                                           <table class="table table-light " style="border: 0.5px solid rgb(230, 229, 229);border-radius: 20px;">
+                                            <tr class="" style="border-bottom: 2px #007A69 solid;">
+                                                <td class="mycolor fw-bold ">Doctor Name</td>
+                                                <td class="mycolor fw-bold">Email</td>
+                                                <td class="mycolor fw-bold">Speciality</td>
+                                                <td class="mycolor fw-bold">Events</td>
+                                            </tr> 
+                                            <tr class="">
+                                                <td class="text-dark">Ossama</td>
+                                                <td class="text-dark">ossama@gmail.com</td>
+                                                <td class="text-dark">cardiac</td>
+                                                <td class="text-dark">
+                                                    <button class="btn mycolor button1 rounded-pill"><i class="mycolor uil uil-eye pe-1"></i>view</button>
+                                                    <button class="btn mycolor button1 rounded-pill "><i class="mycolor uil uil-heart-rate pe-1"></i>Sessions</button>
+                                                </td>
+                                            </tr> 
+                                            <tr class="">
+                                                <td class="text-dark">Ossama</td>
+                                                <td class="text-dark">ossama@gmail.com</td>
+                                                <td class="text-dark">cardiac</td>
+                                                <td class="text-dark">
+                                                    <button class="btn mycolor button1 rounded-pill"><i class="mycolor uil uil-eye pe-1"></i>view</button>
+                                                    <button class="btn mycolor button1 rounded-pill"><i class="mycolor uil uil-heart-rate pe-1"></i>Sessions</button>
+                                                </td>
+                                            </tr>         
+                                           </table>
+                         </div>
+                        
+                   </section>
