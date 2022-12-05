748151fcd5cd0d7384dc2f22b42c6bdcea89467c
Ossama kharbaq <ossamakharbaq4@gmail.com>
2022-12-05 14:30:49 +0100

    this commit is to fix a problem in the workingflow

--- assets/js/sign.js
+++ assets/js/sign.js
@@ -34,30 +34,144 @@
     let birthday    =   $('#birthday');
     let password    =   $('#password');
     let passConf    =   $('#pass-confirmation');
+    let message    =   $('#message');
 
     function checkName(name){
-        return /^[a-z ,.'-]+$/.test(name);
+        return /^[A-Za-z ,.'-]+$/.test(name);
     }
 
     function checkEmail(email){
-        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+        return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{1,4}$/.test(email);
     }
 
     function checkPassword(password){
         return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+        // At Least 8 Characters, One Uppercase, And One Number
     }
 
     function checkCin(cin){
         return /^[A-Za-z0-9]+$/.test(cin);
+        // just numbers and letters
     }
 
-    signUpForm.keyup(function(){
+
+        let result = true;
+        let tester = $('#tester');
+
+
+        function check(x){
+            if(x.val() == '' || !checkName(x.val())){
+                x.addClass('invalid');
+                message.text('Write A Valid First Name');
+
+
+                result = false;
+            }else{
+                x.removeClass('invalid');
+                message.text('');
+
+                result = true;
+            }
+        }
+
         firstName.keyup(function(){
-            if(firstName = ''){
-                
+            if(firstName.val() == '' || !checkName(firstName.val())){
+                firstName.addClass('invalid');
+                message.text('Write A Valid First Name');
+
+
+                result = false;
+            }else{
+                firstName.removeClass('invalid');
+                message.text('');
+
+                result = true;
+            }
+            
+        })
+
+        lastName.keyup(function(){
+            if(lastName.val() == '' || !checkName(lastName.val())){
+                lastName.addClass('invalid');
+                message.text('Write A Valid Last Name');
+
+                result = false;
+            }else{
+                lastName.removeClass('invalid');
+                message.text('');
+
+                result = true;
             }
         })
-        
 
-        console.log('hello every thing is good');
-    })
+        email.keyup(function(){
+            if(email.val() == '' || !checkEmail(email.val())){
+                email.addClass('invalid');
+                message.text('Write A Valid Email');
+
+                result = false;
+            }else{
+                email.removeClass('invalid');
+                message.text('');
+
+                result = true;
+            }
+            tester.text(result);
+        })
+
+
+        cin.keyup(function(){
+            if(cin.val() == '' || !checkCin(cin.val())){
+                cin.addClass('invalid');
+                message.text('CIN most have just letters and numbers');
+
+                result = false;
+            }else{
+                cin.removeClass('invalid');
+                message.text('');
+
+                result = true;
+            }
+        })
+
+        password.keyup(function(){
+            if(password.val() == '' || !checkPassword(password.val())){
+                password.addClass('invalid');
+                message.text('At Least 8 Characters, One Uppercase, And One Number');
+
+                result = false;
+            }else{
+                password.removeClass('invalid');
+                message.text('');
+
+                result = true;
+            }
+        })
+
+        passConf.keyup(function(){
+            if(passConf.val() == '' || passConf.val() != password.val()){
+                passConf.addClass('invalid');
+                message.text('Passwords are not match');
+
+                result = false;
+            }else{
+                passConf.removeClass('invalid');
+                message.text('');
+
+                result = true;
+            }
+        })
+
+        signUpForm.submit(function(){
+            if(birthday.val() == ''){
+                birthday.addClass('invalid');
+                message.text('Enter Your Birthday');
+
+                result = false;
+            }else{
+                birthday.removeClass('invalid');
+                message.text('');
+
+                result = true;
+            }
+        })
--- sign.php
+++ sign.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assets/css/style_admin.css">
+    <link rel="stylesheet" href="./assets/css/style_admin.css">
     
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
@@ -15,30 +15,32 @@
 
     <title>Document</title>
 </head>
+
+<p id="tester"></p>
 <body class="row m-0 p-0 flex-column">
 <div class="col-lg-5 col-md-6 col-11 m-auto d-flex justify-content-between mt-5">
-    <button class="m-auto d-block w-50 p-2 bg-white mycolor myborder border-bottom-0" style="border-radius: 5px 5px 0px 0px ;" id="lg-Btn">
+    <button class="m-auto d-block w-50 p-2 button1 mycolor myborder border-bottom-0" style="border-radius: 5px 5px 0px 0px ;" id="lg-Btn">
         Log In
     </button>
-    <button class="m-auto d-block w-50 p-2 button1 mycolor myborder  border-bottom-0" style="border-radius: 5px 5px 0px 0px ;" id="su-Btn">
+    <button class="m-auto d-block w-50 p-2 bg-white mycolor myborder  border-bottom-0" style="border-radius: 5px 5px 0px 0px ;" id="su-Btn">
         Sign Up
     </button>
 </div>
 
 <!-- log in form starts here  -->
 
-<form action="" class="col-lg-5 col-md-6 col-11 m-auto shadow rounded py-4" id="sign-in">
+<form action="" class="col-lg-5 d-none col-md-6 col-11 m-auto shadow rounded py-4" id="sign-in">
     <div class="w-100 my-3 mb-5">
         <h3 class="text-center">Welcome Back</h3>
         <p class="text-center text-secondary">login with your details to continue</p>
     </div>
     <div class="d-flex flex-column my-3 col-9 m-auto">
         <label class="mb-2" for="email">Email :</label>
-        <input class="border-0 shadow-sm p-2" style="border: 0.3px solid black;" type="email" name="email" id="email" placeholder="Email Address">
+        <input class="border-0 shadow-sm p-2" style="border: 0.3px solid black;" type="email" onkeyup="checkEmail(this);" name="email" id="email" placeholder="Email Address">
     </div>
     <div class="d-flex flex-column my-3 col-9 m-auto">
         <label class="mb-2" for="password" >Password :</label>
-        <input class="border-0 shadow-sm p-2" style="border: 0.3px solid black;" type="password" name="password" id="password" placeholder="Password">
+        <input class="border-0 shadow-sm p-2" style="border: 0.3px solid black;" type="password" onkeyup="checkPassword(this);" name="password" id="password" placeholder="Password">
     </div>
     <div class="w-100 d-flex flex-column justify-content-center">
         <button class="my-2 mb-4 text-center w-50 border-0 rounded button1 mycolor py-1 m-auto">Log In</button>
@@ -51,7 +53,7 @@
 
 <!-- sign up form starts here  -->
 
-<form action="" class="col-lg-5 d-none col-md-6 col-11 m-auto shadow rounded py-2" id="sign-up">
+<form action="" class="col-lg-5 col-md-6 col-11 m-auto shadow rounded py-2" id="sign-up">
     <div class="w-100 my-3 mb-2">
         <h3 class="text-center">Let's Get Started</h3>
         <p class="text-center text-secondary">add your personal details to continue</p>
@@ -59,31 +61,32 @@
     <div class="d-flex flex-column my-2 col-9 m-auto">
         <label class="mb-2" for="email">Name :</label>
         <div class="w-100 d-flex">
-            <input class="border-0 shadow-sm p-2 col-6 me-2 invalid" style="border: 0.3px solid black;" type="name1" name="name1" id="name1" placeholder="First Name">
-        <input class="border-0 shadow-sm p-2 col-6" style="border: 0.3px solid black;" type="name2" name="name2" id="name2" placeholder="Last Name">
+            <input class="border-0 shadow-sm p-2 col-6 me-2" type="name1" name="name1" id="name1" onkeyup="checkName(this);" placeholder="First Name">
+        <input class="border-0 shadow-sm p-2 col-6" type="name2" name="name2" id="name2" onkeyup="checkName(this);" placeholder="Last Name">
         </div>
         
     </div>
     <div class="d-flex flex-column my-2 col-9 m-auto">
         <label class="mb-2" for="email">Email :</label>
-        <input class="border-0 shadow-sm p-2" style="border: 0.3px solid black;" type="email" name="email" id="email" placeholder="Email Address">
+        <input class="border-0 shadow-sm p-2" type="email" name="email" id="email" onkeyup="checkEmail(this);" placeholder="Email Address">
     </div>
     <div class="d-flex flex-column my-2 col-9 m-auto">
         <label class="mb-2" for="cin" >CIN :</label>
-        <input class="border-0 shadow-sm p-2" style="border: 0.3px solid black;" type="text" name="cin" id="cin" placeholder="CIN">
+        <input class="border-0 shadow-sm p-2" type="text" name="cin" id="cin" onkeyup="checkCin(this);" placeholder="CIN">
     </div>
     <div class="d-flex flex-column my-3 col-9 m-auto">
         <label class="mb-2" for="birthday" >Date Of Birth :</label>
-        <input class="border-0 shadow-sm p-2" style="border: 0.3px solid black;" type="date" name="birthday" id="birthday">
+        <input class="border-0 shadow-sm p-2" type="date" name="birthday" id="birthday">
     </div>
     <div class="d-flex flex-column my-3 col-9 m-auto">
         <label class="mb-2" for="password" >Password :</label>
-        <input class="border-0 shadow-sm p-2" style="border: 0.3px solid black;" type="password" name="password" id="password" placeholder="At Least 8 Characters, One Uppercase, And One Number">
+        <input class="border-0 shadow-sm p-2" type="password" name="password" id="password" onkeyup="checkPassword(this);" placeholder="At Least 8 Characters, One Uppercase, And One Number">
     </div>
     <div class="d-flex flex-column my-3 col-9 m-auto">
         <label class="mb-2" for="pass-confirmation" >Password Confirmation :</label>
-        <input class="border-0 shadow-sm p-2" style="border: 0.3px solid black;" type="password" name="pass-confirmation" id="pass-confirmation" placeholder="Repeat Your Password">
+        <input class="border-0 shadow-sm p-2" type="password" name="pass-confirmation" id="pass-confirmation" onkeyup="checkPass2(this);" placeholder="Repeat Your Password">
     </div>
+    <p id="message" class="text-danger text-center" style="font-size: 10px; margin-bottom: 0px;"></p>
     <div class="w-100 d-flex flex-column justify-content-center">
         <div class="d-flex justify-content-between col-9 m-auto">
             <button style="width: 45%;" class="my-2 mb-4 text-center bg-white  myborder rounded py-1 mycolor  m-auto">Reset</button>
